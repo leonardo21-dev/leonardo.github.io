0c31dc7055b24309581d81d6c3e37ac48c939ae0
leonardo21-dev <Leonardoalves18leo@gmail.com>
2025-07-21 21:33:23 -0300

    editing files js, css and index

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,46 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+let elemento = document.querySelector('.titles-area h1');
+elemento.addEventListener('click', ()=> {
+  if (window.innerWidth > 850) {
+    if (elemento.classList.contains('ativa')) {
+      elemento.classList.remove('ativa');
+      elemento.classList.add('h1');
+    } else {
+      elemento.classList.remove('h1');
+      elemento.classList.add('ativa');
+    }
+  } 
+});
+
+let elemento2 = document.querySelector('.titles-area p');
+elemento2.addEventListener('click', ()=> {
+  if (window.innerWidth > 850) {
+    if (elemento2.classList.contains('pativa')) {
+      elemento2.classList.remove('pativa');
+      elemento2.classList.add('p');
+    } else {
+      elemento2.classList.remove('p');
+      elemento2.classList.add('pativa');
+    }
+  } 
+});
+
+let skills = document.querySelectorAll('.skill-area-item');
+skills.forEach((skill) => {
+  skill.addEventListener('click', ()=> {
+    if(window.innerWidth > 850) {
+      if (skill.classList.contains('skill-area-item-x')) {
+        skill.classList.remove('skill-area-item-x');
+        skill.classList.add('skill-area-item');
+      } else {
+        skill.classList.remove('skill-area-item');
+        skill.classList.add('skill-area-item-x')
+      }
+    }
+  })
+});
+
+
